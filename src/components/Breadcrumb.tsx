575de5bc08366f23125ea36496e914387fb24da9
hefkay <hefkay.lawal2019@gmail.com>
2024-10-14 18:06:56 -0700

    pushed to production

--- src/components/Breadcrumb.tsx
+++ src/components/Breadcrumb.tsx
@@ -2,6 +2,8 @@ import { Link } from 'react-router-dom';
 interface BreadcrumbProps {
   pageName: string;
 }
+
+
 const Breadcrumb = ({ pageName }: BreadcrumbProps) => {
   return (
     <div className="mb-6 flex flex-col gap-3 sm:flex-row sm:items-center sm:justify-between">
